63ef5a91647155f97c20356c07caaf6a6ab8f860
admin <admin@BCS-Ubuntu-30.lrccd.ad.losrios.edu>
2022-09-28 10:19:48 -0700

    makefile

---
+++ Mandle P/Code/ComplexPlane.h
@@ -0,0 +1,20 @@
+#ifndef COMPLEXPLANE_H
+#define COMPLEXPLANE_H
+
+class Zoom 
+{
+    public:
+
+    
+    private:
+    const unsigned int MAX_ITER = 64;
+    const float BASE_WIDTH = 4.0;
+    const float BASE_HEIGHT = 4.0;
+    const float BASE_ZOOM = 0.5;
+};
+
+
+
+
+
+#endif
