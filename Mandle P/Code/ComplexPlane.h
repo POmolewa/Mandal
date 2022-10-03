f6b829b8d02c99d0acddee9b36ced077d2bd7779
Pius Omolewa <W1978720@apps.losrios.edu>
2022-10-02 19:27:53 -0700

    Mostly done

--- Mandle P/Code/ComplexPlane.h
+++ Mandle P/Code/ComplexPlane.h
@@ -1,17 +1,56 @@
 #ifndef COMPLEXPLANE_H
 #define COMPLEXPLANE_H
+#include <SFML/Graphics.hpp>
+#include <iostream>
 
-class Zoom 
+using namespace sf;
+using namespace std;
+
+class ComplexPlane 
 {
     public:
+        void SetCenterMouseCoords(double x, double y);
+        void Initialize();
         int getZoom();
+        void 
+            ZoomIn(RectangleShape),
+            ZoomIn2(RectangleShape),
+            ZoomOut(RectangleShape, RectangleShape);
+        double MakeReal(double value, double localMin, double localMax, double min, double max);
+        double FindMandle(double value, double localMin);
+        double
+            get_Rx(),
+            get_Ry(),
+            get_Rw(),
+            get_Rh();
+        string LoadText();
 
-    
+
     private:
     const unsigned int MAX_ITER = 64;
     const float BASE_WIDTH = 4.0;
     const float BASE_HEIGHT = 4.0;
     const float BASE_ZOOM = 0.5;
+
+    int width = VideoMode::getDesktopMode().width;
+    int height = VideoMode::getDesktopMode().height;
+
+    double 
+        Xmin = -4.0,
+        XMax = 4.0,
+        Ymin = ((-1 * (abs(Xmin) + abs(XMax)) * height / width)) / 2,
+        YMax = ((abs(Xmin) + abs(XMax)) * height / width) / 2;
+
+    double
+        MouseX,
+        MouseY,
+        RealX,
+        RealY,
+        RealX2,
+        RealY2,
+        RealWidth,
+        RealHeight;
+
 };
 
 
--- Mandle P/Code/Main.cpp
+++ Mandle P/Code/Main.cpp
@@ -1,20 +1,18 @@
 #include <SFML/Graphics.hpp>
-#include <math.h>
+#include <cmath>
 #include <iostream>
 #include "ComplexPlane.h"
 
-
-
-double RealValue(double value, double localMin, double localMax, double min, double max);
-double FindMandle(double cx, double cy, int maxIter);
 sf::Texture mandelbrot(int width, int height, double xmin, double xmax, double ymin, double ymax, int iterations);
+ComplexPlane Plane;
 
 using namespace sf;
 using namespace std;
 int main()
 {
-	 int width = VideoMode::getDesktopMode().width;
-	 int height = VideoMode::getDesktopMode().height;
+	bool in = true;
+	int width = VideoMode::getDesktopMode().width;
+	int height = VideoMode::getDesktopMode().height;
 
 	RenderWindow window(sf::VideoMode(width, height), "Project");
 
@@ -27,33 +25,22 @@ int main()
 	zoomBorder.setOutlineThickness(1.0f);
 	zoomBorder.setOrigin(Vector2f(zoomBorder.getSize().x / 2, zoomBorder.getSize().y / 2));
 
-	double oxmin = -4.0;
-	double oxmax = 4.0;
-	double oyRange = (abs(oxmin) + abs(oxmax)) * height / width;
-	double oymin = -oyRange / 2;
-	double oymax = oyRange / 2;
+	RectangleShape zoomBorder2(Vector2f(width * 2, height * 2));
+	zoomBorder2.setFillColor(Color(0, 0, 0, 0));
+	zoomBorder2.setOutlineColor(Color(0, 126, 126, 126));
+	zoomBorder2.setOutlineThickness(1.0f);
+	zoomBorder2.setOrigin(Vector2f(zoomBorder2.getSize().x / 2, zoomBorder2.getSize().y / 2));
+	
+	Plane.Initialize();
+	mandelTexture = mandelbrot(width, height, Plane.get_Rx(), Plane.get_Rw(), Plane.get_Ry(), Plane.get_Rh(), Plane.getZoom());
 
-	double xmin = oxmin;
-	double xmax = oxmax;
-	double yRange = oyRange;
-	double ymin = oymin;
-	double ymax = oymax;
-
-	//int MaxIterations = 64;
-	int recLevel = 1;
-	Zoom Zoomwindow; // Remove
-	mandelTexture = mandelbrot(width, height, oxmin, oxmax, oymin, oymax, Zoomwindow.getZoom());
-
-	sf::Font font;
-	//put a specific font font.loadFromFile("");
-
-	sf::Text zoomText, precText;
-	zoomText.setFont(font);
-	precText.setFont(font);
-	zoomText.setFillColor(sf::Color::White);
-	precText.setFillColor(sf::Color::White);
-	zoomText.setCharacterSize(24);
-	precText.setCharacterSize(24);
+	Font font;
+	font.loadFromFile("fonts/KOMIKAP_.ttf");
+	Text CenterText;
+	CenterText.setFont(font);
+	CenterText.setFillColor(sf::Color::White);
+	CenterText.setCharacterSize(24);
+	
 
 	while (window.isOpen())
 	{
@@ -63,78 +50,49 @@ int main()
 			
 			if (evnt.type == Event::Closed)
             {
-                window.close();															//remove
-				mandelTexture = mandelbrot(width, height, xmin, xmax, ymin, ymax, Zoomwindow.getZoom());
+                window.close();
 			}
 		}
-
+		
 		if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
 		{
-			recLevel++;
-
-			double x = zoomBorder.getPosition().x - zoomBorder.getSize().x / 2;
-			double y = zoomBorder.getPosition().y - zoomBorder.getSize().y / 2;
-
-			double x2 = x + zoomBorder.getSize().x;
-			double y2 = y + zoomBorder.getSize().y;
-
-			//from px range to grid range
-			double Realx = RealValue(x, 0.0, width, xmin, xmax);
-			double Realy = RealValue(y, 0.0, height, ymin, ymax);
-
-			double widthNorm = RealValue(x2, 0.0, width, xmin, xmax);
-			double heightNorm = RealValue(y2, 0.0, height, ymin, ymax);
-
-			xmin = Realx;
-			xmax = widthNorm;
-			ymin = Realy;
-			ymax = heightNorm;
-
-			mandelTexture = mandelbrot(width, height, xmin, xmax, ymin, ymax, Zoomwindow.getZoom());
+			Plane.ZoomIn(zoomBorder);
+			mandelTexture = mandelbrot(width, height, Plane.get_Rx(), Plane.get_Rw(), Plane.get_Ry(), Plane.get_Rh(), Plane.getZoom());
+		}
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
+		{
+			in = false;
+			Plane.ZoomOut(zoomBorder2, zoomBorder);
+			mandelTexture = mandelbrot(width, height, Plane.get_Rx(), Plane.get_Rw(), Plane.get_Ry(), Plane.get_Rh(), Plane.getZoom());
+			
 		}
 
-		zoomText.setString("Zoom: " + std::to_string(pow(8, recLevel - 1)));
-		precText.setString("Max. Iterations: " + std::to_string(Zoomwindow.getZoom()));
-		precText.setPosition(Vector2f(0, 32));
-
-		zoomBorder.setPosition(Mouse::getPosition(window).x,Mouse::getPosition(window).y);
+		Plane.ZoomIn2(zoomBorder);
+		CenterText.setPosition(Vector2f(0, 32));
+		CenterText.setString(Plane.LoadText());
+		
+		zoomBorder.setPosition(Mouse::getPosition(window).x, Mouse::getPosition(window).y);
 
 		mandelSprite.setTexture(mandelTexture);
 
 		window.clear(sf::Color::White);
-
 		window.draw(mandelSprite);
-		window.draw(zoomText);
-		window.draw(precText);
-		window.draw(zoomBorder);
+		window.draw(CenterText);
+
+
+		if (in == true)
+		{
+			window.draw(zoomBorder);
+		}
+		else
+		{
+			window.draw(zoomBorder2);
+		}
 
 		window.display();
 	}
 
 	return 0;
-}
-
-double RealValue(double value, double localMin, double localMax, double min, double max)
-{
-	double Real = (value - localMin) / (localMax - localMin);
-	Real = Real * (max - min);
-	Real += min;
-	return Real;
-}
-
-double FindMandle(double cx, double cy, int maxIter) 
-{
-	int i = 0;
-	double zr = 0.0, zi = 0.0;
-	while (i < maxIter && zr * zr * zi < 4.0)
-	{
-		double temp = zr * zr - zi * zi + cx;
-		zi = 2.0 * zr * zi + cy;
-		zr = temp;
-		i++;
-	}
-	return i;
-
 }
 
 Texture mandelbrot(int width, int height, double xmin, double xmax, double ymin, double ymax, int iterations)
@@ -151,13 +109,13 @@ Texture mandelbrot(int width, int height, double xmin, double xmax, double ymin,
 			double x = xmin + (xmax - xmin) * index / (width - 1.0);
 			double y = ymin + (ymax - ymin) * i / (height - 1.0);
 
-			int Ipoint = FindMandle(x, y, iterations);
+			int Ipoint = Plane.FindMandle(x, y);
 
 			int ppos = 4 * (width * i + index);
 
 			int hue = ((Ipoint * Ipoint) % 256);
-			int sat = ((Ipoint * Ipoint) % 256);
-			int val = (Ipoint % 256);
+			int sat = ((sqrt(static_cast<int>(Ipoint) % 256)));
+			int val = ((Ipoint * Ipoint) % 256);
 
 			Color hsvtorgb = Color (hue, sat, val);
 			pixels[ppos] = (int)hsvtorgb.r;
